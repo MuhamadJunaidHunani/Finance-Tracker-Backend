c9443cf4042e9aba4ac32a52efac439dcdbab020
MuhamadJunaidHunani <junaidhunani890@gmail.com>
2024-12-28 11:55:20 +0500

    create auth apis

--- Controllers/AuthApis/Register.js
+++ Controllers/AuthApis/Register.js
@@ -1,50 +1,47 @@
 // register user
 const registerUser = async (req, res) => {
-    const { email, password, userName } = req.body;
-  
-    try {
-      const existingUser = await UserModal.findOne({
-        $or: [{ email }, { nickName }],
-      });
-  
-      if (existingUser) {
-        const message =
-          existingUser.email === email
-            ? "Email already exists"
-            : "NickName already exists";
-        return res.status(409).json({ message });
-      }
-  
-      const hashedPassword = await bcrypt.hash(password, 10);
-  
-      const fileData = {
-        file: req.file.location,
-        name: req.file.originalname,
-        timestamp: Date.now(),
-        type: req.file.mimetype,
-      };
-  
-      const newUser = new UserModal({
-        ...req.body,
-        email,
-        password: hashedPassword,
-        nickName,
-        currentWeekPdf: fileData,
-        pdfs: [fileData],
-      });
-  
-      const savedUser = await newUser.save();
-  
-      savedUser.password = undefined;
-  
-      return res.status(201).json(savedUser);
-    } catch (error) {
-      console.error("Error during user registration:", error.message);
-      return res
-        .status(500)
-        .json({ message: "An error occurred during registration." });
+  const { email, password, userName } = req.body;
+
+  try {
+    const existingUser = await UserModal.findOne({ email });
+
+    if (existingUser) {
+      const message =
+        existingUser.email === email
+          ? "Email already exists"
+          : "NickName already exists";
+      return res.status(409).json({ message });
     }
-  };
-  
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const fileData = {
+      file: req.file.location,
+      name: req.file.originalname,
+      timestamp: Date.now(),
+      type: req.file.mimetype,
+    };
+
+    const newUser = new UserModal({
+      ...req.body,
+      email,
+      password: hashedPassword,
+      nickName,
+      currentWeekPdf: fileData,
+      pdfs: [fileData],
+    });
+
+    const savedUser = await newUser.save();
+
+    savedUser.password = undefined;
+
+    return res.status(201).json(savedUser);
+  } catch (error) {
+    console.error("Error during user registration:", error.message);
+    return res
+      .status(500)
+      .json({ message: "An error occurred during registration." });
+  }
+};
 
 module.exports = registerUser;
